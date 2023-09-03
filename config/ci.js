b5535a13ec24bd6e1ecc1a327b8fcb4253f9df9a
mehmet <sayinmehmet47@gmail.com>
2023-09-03 22:57:13 +0200

    feat: add ci

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '1063437447856-9pbigtnc8c2ua3gf7n8ph8abj1p5v51c.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+};
